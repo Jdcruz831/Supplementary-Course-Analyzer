3cb8901be408962cdb311773ba09000cae8c9e62
Jdcruz831 <jacob.cruz.9353@gmail.com>
2024-03-06 22:02:35 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@emotion/react';
+import Banner from './Reusables/banner';
+
+
+const theme = {
+  colors: {
+    primary: '#043927',
+    secondary: '#c4b581',
+  },
+};
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Banner></Banner>
+        
+        <div style={{ position: 'absolute', bottom: 0, right: 0, width: '50vw', height: '58%', backgroundImage: 'linear-gradient(to bottom, #008453, #043927' }}></div>
       </header>
     </div>
   );
