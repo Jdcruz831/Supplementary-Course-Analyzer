d972992b8a4983248ed6e5886d0c3d2f0db99ef1
Ian Charamuga <iancharamuga@csus.edu>
2024-03-20 22:29:57 -0700

    Added about section; changed banner sizing
    Added:
    - "About Course Analyzer" section with lorem ipsum text
    Changed:
    - Banner size now remains consistent across different screen sizes; fixes an issue with the banner text running off of the background image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider } from '@emotion/react';
 import Banner from './Reusables/banner';
+import { Divider, Typography, colors } from '@mui/material';
 
 
 const theme = {
@@ -14,14 +15,24 @@ const theme = {
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={styles}>
       <header className="App-header">
         <Banner></Banner>
         
-        <div style={{ position: 'absolute', bottom: 0, right: 0, width: '50vw', height: '58%', backgroundImage: 'linear-gradient(to bottom, #008453, #043927' }}></div>
+        <div style={{ position: 'absolute', bottom: 0, right: 0, width: '50vw', height: '58%', backgroundImage: 'linear-gradient(to bottom, #008453, #043927' }}>
+          <div style={{ padding: 20 }}>
+            <Typography variant='h5' fontFamily= 'BlinkMacSystemFont'style={{color: 'rgba(255, 255, 255, 1)',textShadow: '#222 2px 2px' }}>About Course Analyzer</Typography>
+            <Divider style={{ margin: 10 }} />
+            <Typography variant='p' style={{color: '#DDE', textShadow: '#252 2px 2px'}}>Adipisicing duis est nulla et labore proident excepteur. Cillum elit laborum ex sit proident deserunt cupidatat anim mollit eiusmod exercitation elit Lorem. Commodo officia anim quis enim exercitation consequat duis magna anim ea laborum. Non laborum voluptate sint laboris ut laboris aliquip nisi in nisi proident velit. Dolor elit Lorem elit adipisicing deserunt sunt incididunt magna quis occaecat occaecat deserunt ut. Irure irure duis laborum eiusmod consectetur amet nulla deserunt dolore dolore incididunt culpa culpa. Culpa laborum laboris ut Lorem pariatur aliquip veniam occaecat reprehenderit id nisi et duis fugiat.</Typography>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
+
+const styles = {
+  fontFamily: 'BlinkMacSystemFont'
+};
 
 export default App;
--- src/Reusables/banner.js
+++ src/Reusables/banner.js
@@ -6,10 +6,10 @@ import { Typography } from '@mui/material';
 const Banner = ({ color }) => {
   return (
 
-    <div style={{ width: '100vw', marginTop: '5vw', height: '15vw', position: 'relative' }}>
-      <img src={Ginkgo} alt="Ginkgo" style={{ width: '100%', height: '15vw', objectFit: 'cover' }} /> 
-      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundImage: 'linear-gradient(to right, #043927, rgba(255, 255, 255, 0))' }}></div>
-        <div style={{display: 'flex', flexDirection: 'column',  position: 'absolute', top: '4vw', }}> 
+    <div style={{ width: '100vw', marginTop: '20px', height: '15vw', position: 'relative' }}>
+      <img src={Ginkgo} alt="Ginkgo" style={{ minWidth: '100%', height: '180px', objectFit: 'cover' }} /> 
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '180px', backgroundImage: 'linear-gradient(to right, #043927, rgba(255, 255, 255, 0))' }}></div>
+        <div style={{display: 'flex', flexDirection: 'column',  position: 'absolute', top: '30px', }}> 
         <Typography variant="h4"  align="left" fontFamily= 'BlinkMacSystemFont'style={{color: 'rgba(255, 255, 255, 1)',textShadow: 'black 2px 2px' }}>
         Supplementary <br/> Course Analyzer
             </Typography>
