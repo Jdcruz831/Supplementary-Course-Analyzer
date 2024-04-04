3014b12ad22dcb943d0ec30885302315d095f195
shajaatali <75504740+shajaatali@users.noreply.github.com>
2024-04-04 16:07:56 -0700

    reorganizing files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider } from '@emotion/react';
-import Banner from './Reusables/banner';
-
-
-const theme = {
-  colors: {
-    primary: '#043927',
-    secondary: '#c4b581',
-  },
-};
-
+import Banner from './Reusables/Banner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Banner></Banner>
-        
-        <div style={{ position: 'absolute', bottom: 0, right: 0, width: '50vw', height: '58%', backgroundImage: 'linear-gradient(to bottom, #008453, #043927' }}></div>
-      </header>
+     <div>
+      <Banner />
     </div>
-  );
+  
+  )
 }
 
 export default App;
---
+++ src/Pages/Login/index.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import "./Login.css";
+import backgroundImage from ".img/background.jpg";
+import loginIcon from ".img/login-icon.png";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginClicked, setLoginClicked] = useState(false);
+
+  const handleLogin = () => {
+    setLoginClicked(true);
+  };
+
+  return (
+    <div className="login-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      ></div>
+      <div className="login-form">
+        <div className="login-header">
+          <img src={loginIcon} alt="Login Icon" className="login-icon" />
+        </div>
+
+        <div className="login-box">
+          <div className="input-container">
+            <label htmlFor="errorMessage"></label>
+            {/* Show error message only when loginClicked is true */}
+            {loginClicked && !username && !password && (
+              <text className="error-text">Please enter username and password</text>
+            )}
+          </div>
+
+          <div className="input-group">
+            <div className="input-container">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                style={{ 
+                  borderColor: loginClicked && !username && "red",
+                  borderWidth: loginClicked && !username && "2px" // Increase border width
+                }}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="input-container">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                style={{ 
+                  borderColor: loginClicked && !password && "red",
+                  borderWidth: loginClicked && !password && "2px" // Increase border width
+                }}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="input-container">
+          <label htmlFor="loginButton"></label>
+            <button style={{ background: '#0FA153' }} onClick={handleLogin}>Login</button>
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="managepassword"></label>
+            <text
+              className="error-text"
+              type="text"
+              id="managePassword"
+            >
+              Manage Password
+            </text>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
