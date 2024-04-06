c7a09f95e7b9362528fe342655d3b9783100e4b2
Shaquan Kelly <skelly@ppas.com>
2024-04-06 11:17:59 -0700

    Pushing local changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-
-import './App.css';
-import { ThemeProvider } from '@emotion/react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-
-//imports from diffrent files
-import HomeMainPage from './Pages/HomePage/HomeMainPage' 
-import CourseTimeAnalyzerMainPage from './Pages/CourseTimeAnalyzerPage/CourseTimeAnalyzerMainPage' 
-import CreatorsMainPage from './Pages/CreatorsPage/CreatorsMainPage' 
-import SourceDataMainPage from './Pages/SourceDataPage/SourceDataMainPage' 
-import StudentEnrolmentAnalyzerMainPage from './Pages/StudentEnrolmentAnalyzerPage/StudentEnrolmentAnalyzerMainPage' 
-import SupCourseAnalyzerMainPage from './Pages/SupCourseAnalyzer/SupCourseAnalyzerMainPage' 
-import LoginMainPage from './Pages/LoginPage/LoginMainPage';
-import AboutMainPage from './Pages/AboutPage/AboutMainPage';
-
+import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import Layout from "./components/layout";
+import RouteProvider from "./utils/routes";
 
 function App() {
   return (
-     <div>
-      
-
-      <HashRouter>
-      <Routes>
-        <Route path="/" element={<HomeMainPage />} />
-        <Route path="/CourseTimeAnalyzer" element={<CourseTimeAnalyzerMainPage />} />
-        <Route path="/Creators" element={<CreatorsMainPage />} />
-        <Route path="/SourceData" element={<SourceDataMainPage />} />
-        <Route path="/StudentEnrolmentAnalyzer" element={<StudentEnrolmentAnalyzerMainPage />} />
-        <Route path="/SupCourseAnalyzer" element={<SupCourseAnalyzerMainPage />} />
-        <Route path="/Login" element={<LoginMainPage />} />
-        <Route path="/About" element={<AboutMainPage />} />
-
-      </Routes>
-    </HashRouter>
-
+    <div>
+      <Layout>
+        <RouteProvider/>
+      </Layout>
     </div>
-  
-  )
+  );
 }
 
 export default App;
---
+++ src/theme/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#043927",
+      stinger: "#008453",
+      evergreen: "#00aa4f",
+      spruce: "#58b947",
+    },
+    secondary: {
+      main: "#c4b581",
+      birch: "#dad490",
+      hornet: "#e6b711",
+    },
+  },
+});
