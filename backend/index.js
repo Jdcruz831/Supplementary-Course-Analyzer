5806ed23ce2e0f862a41d5300235c24d8cbc2f2f
chidoandrus <chidoandrus@csus.edu>
2024-04-09 22:58:26 -0700

    Set up backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+S
