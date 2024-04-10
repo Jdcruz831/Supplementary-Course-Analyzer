acd0755a9461a9706ce6b10b644bf57391dbb602
chidoandrus <chidoandrus@csus.edu>
2024-04-09 23:58:44 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
-
-app.use(express.json());
+const port = 3002;
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${port}`);
 });
