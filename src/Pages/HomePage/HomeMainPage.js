7170ae23418c237a4e0a0982395cfa876d74fcb2
NotRajP <rajpannu8898@gmail.com>
2024-04-10 21:58:17 -0700

    #Remove broken banner from all pages and revert HomePage back to previous version

--- src/Pages/HomePage/HomeMainPage.js
+++ src/Pages/HomePage/HomeMainPage.js
@@ -56,7 +56,17 @@ function HomeMainPage() {
 
 
 
-          <div>
+          <div style={{ width: '100vw', marginTop: '5vw', height: '15vw', position: 'relative' }}>
+      <img src={Ginkgo} alt="Ginkgo" style={{ width: '100%', height: '15vw', objectFit: 'cover' }} /> 
+      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundImage: 'linear-gradient(to right, #043927, rgba(255, 255, 255, 0))' }}></div>
+        <div style={{display: 'flex', flexDirection: 'column',  position: 'absolute', top: '4vw', }}> 
+        <Typography variant="h4"  align="left" fontFamily= 'BlinkMacSystemFont'style={{color: 'rgba(255, 255, 255, 1)',textShadow: 'black 2px 2px' }}>
+        Supplementary <br/> Course Analyzer
+            </Typography>
+            <Typography variant="h5"  align="left" fontFamily= 'BlinkMacSystemFont'style={{ color: 'rgba(196, 181, 129, 1)',textShadow: 'black 2px 2px' }}>
+        Sacramento State
+            </Typography>
+            </div>
     </div>
   
 
