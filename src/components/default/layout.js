51d0ee60c2eae02669739014d1b510ce2bf46df9
Shaquan Carolina <skellypfmgmt+vercel@gmail.com>
2024-04-18 09:53:22 -0700

    [feat] Added search page with working filtering functionality.

--- src/components/default/layout.js
+++ src/components/default/layout.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Footer } from "./footer";
 import { TopNav } from "./top-nav";
+import { Grid } from "@mui/material";
 
 const DefaultLayout = ({ children }) => {
   return (
     <div>
       <TopNav />
-      {children}
+      <Grid container justifyContent="center">
+        <Grid item sx={{ width: "1140px" }}>
+          {children}
+        </Grid>
+      </Grid>
       {/* <Footer /> */}
     </div>
   );
