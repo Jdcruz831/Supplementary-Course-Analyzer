a45df93f35664b0b3cad45e65559e1fa90ab0cdb
Shaquan Kelly <skelly@ppas.com>
2024-10-01 19:13:57 -0700

    Committing local changes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDvEjPI_iBmEdCg2PZ3zK_1tjDILfexkfU",
-  authDomain: "pal-analyzer.firebaseapp.com",
-  projectId: "pal-analyzer",
-  storageBucket: "pal-analyzer.appspot.com",
-  messagingSenderId: "851912563710",
-  appId: "1:851912563710:web:53735094fc1648f436feaf",
-  measurementId: "G-K98WWM6XKT",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -22,4 +24,5 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export default app;
+export const db = getFirestore(app);
 export const auth = getAuth(app);
