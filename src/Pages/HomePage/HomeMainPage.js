25d58986e5ffe5ada4015b990b2bf3cc709a83e0
shajaatali <75504740+shajaatali@users.noreply.github.com>
2024-10-01 19:07:03 -0700

    DT-104

--- src/Pages/HomePage/HomeMainPage.js
+++ src/Pages/HomePage/HomeMainPage.js
@@ -11,12 +11,12 @@ function HomeMainPage() {
     '&:hover': {
       backgroundColor: 'rgba(0, 100, 0, 0.7)', // Dark green on hover
     },
-    fontWeight: "bold",
+    fontWeight: 'medium',
+    fontSize:"1.2em",
     boxShadow: "inset 0 -3px 0 rgba(0,0,0,0.2)",
     bgcolor: "#0c3a2b", // Dark background for buttons
     color: "white", // Text color
     width: '100%',
-    borderRadius: '4px',
   };
 
   return (
@@ -27,12 +27,13 @@ function HomeMainPage() {
           flexDirection: {
             xs: "column",
             md: "row",
+            background:'#D8D2BA',
           },
         }}
       >
         <Grid item md={6} sx={{ p: 8 }} container>
           <Stack direction="column" spacing={0.5}>
-            <Typography variant="h6" fontWeight="bold" bgcolor={(theme) => theme.palette.secondary.hornet} p={2} borderRadius={1} textAlign="left">
+            <Typography variant="h6" fontWeight="bold" bgcolor='#E6DF79' p={2} borderRadius={1} textAlign="left">
               Access Analyzer
             </Typography>
             <Button component="a" href="/#/search" sx={buttonStyle}>
@@ -44,7 +45,7 @@ function HomeMainPage() {
             <Button component="a" href="/#/CourseTimeAnalyzer" sx={buttonStyle}>
               Course Time Analyzer
             </Button>
-            <Typography variant="h6" fontWeight="bold" bgcolor={(theme) => theme.palette.secondary.hornet} p={2} borderRadius={1} textAlign="left">
+            <Typography variant="h6" fontWeight="bold" bgcolor='#E6DF79' p={2} borderRadius={1} textAlign="left">
               About Analyzer
             </Typography>
             <Button component="a" href="/#/Creators" sx={buttonStyle}>
@@ -69,17 +70,17 @@ function HomeMainPage() {
           }}
         >
           <Stack spacing={3}>
-            <Typography variant="h3" sx={{ color: "white" }}>
+            <Typography variant="h3" sx={{ fontSize: "3.0em", fontWeight:'medium',color: '#E6DF79' }}>
               About Our Course Analyzer
             </Typography>
-            <Typography variant="body1" sx={{ color: "white", fontSize: "1.2em" }}>
+            <Typography variant="body1" sx={{ color: "white", fontSize: "1.6em" }}>
               The CSUS PAL program aims to support students in challenging STEM courses by
             offering supplementary classes led by former students. These classes, held in person, require
             campus classrooms that do not conflict with core STEM courses. However, identifying suitable
             times and classrooms poses a challenge due to the lack of software to analyze scheduling data for
             STEM courses.
           </Typography>
-          <Typography variant="body1" sx={{ color: "white", fontSize: "1.2em" }}>
+          <Typography variant="body1" sx={{ color: "white", fontSize: "1.6em" }}>
             The Supplementary Course Analyzer aims to solve this problem by providing a website that will analyze course times and 
             determine optimal times for supplementary courses, while minimizing interference with major courses. We aim to make PAL courses 
             more accessible to students and help the PAL program accomplish their goal of helping students learn.
